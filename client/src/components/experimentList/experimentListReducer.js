239ee5d586af8e441d139b2a6d6a95e43078af07
Piotrek <pplonski86@gmail.com>
2018-12-20 13:14:45 +0100

    experiment list

---
+++ client/src/components/experimentList/experimentListReducer.js
@@ -0,0 +1,39 @@
+import {
+  EXPERIMENTS_LOADING,
+  GET_EXPERIMENTS_SUCCESS,
+  GET_EXPERIMENTS_ERROR
+} from "./experimentListTypes";
+
+const initialState = {
+  experiments: [],
+  loading: false,
+  error_message: ""
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case EXPERIMENTS_LOADING:
+      return {
+        ...state,
+        experiments: [],
+        loading: true,
+        error_message: ""
+      };
+    case GET_EXPERIMENTS_SUCCESS:
+      return {
+        ...state,
+        experiments: action.payload,
+        loading: false,
+        error_message: ""
+      };
+    case GET_EXPERIMENTS_ERROR:
+      return {
+        ...state,
+        experiments: [],
+        loading: false,
+        error_message: action.error_message
+      };
+    default:
+      return state;
+  }
+}
